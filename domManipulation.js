e1e92286545f78d271c0d329ef103f7ce606af13
saltedmuse <eliizavetasereda@gmail.com>
2024-06-13 13:51:00 -0400

    button

--- domManipulation.js
+++ domManipulation.js
@@ -22,22 +22,24 @@ let image = document.querySelector('.image');
 //   - The `location` paragraph.
 
 contentButton.addEventListener('click', function() {
-
-
-
-
-
-
+myName.innerHTML="Lizy";
+myHobby.innerHTML="draw";
+myLocation.innerHTML="my house"
 });
 
 // 3. Add an event listener to the `image1Button`
 // 4. When clicked, it should change the source of `image` to another image URL.
-
+image1Button.addEventListener("click", function() {
+    image.src="https://i.pinimg.com/474x/dc/2d/29/dc2d2937f49a65a2ecb3cc01667cb8f0.jpg"
+});
 
 
 
 
 // 5. Add an event listener to the `image2Button`
+image2Button.addEventListener("click", function () {
+image.src="https://i.pinimg.com/474x/63/39/8f/63398fed5b53ec9ac7897ea899e3e17c.jpg"
+})
 // 6. When clicked, it should change the source of `image` to another image URL.
 
 
